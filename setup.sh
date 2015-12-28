c13cf9070147ae269f5af3c3aaecaa386c336d90
Harmish Khambhaita <harmish@laas.fr>
2015-12-28 17:04:30 +0100

    making the setup script run from any directory

--- setup.sh
+++ setup.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 # set direcoty paths in bing-cron and bing-wallpaper.sh
-PWD="`pwd`"
-gawk "{ gsub(\"/DOWNLOAD_DIR\", \"$PWD\",\$0); print \$0 }" bing-cron > tmp.tmp
-mv tmp.tmp bing-cron
-gawk "{ gsub(\"/DOWNLOAD_DIR\", \"$PWD\",\$0); print \$0 }" bing-wallpaper.sh > tmp.tmp
-mv tmp.tmp bing-wallpaper.sh
-chmod a+x bing-wallpaper.sh
+if [ $# -eq 0 ]; then
+    PWD=`pwd`
+else
+    PWD=$1
+fi
+gawk "{ gsub(\"/DOWNLOAD_DIR\", \"${PWD}\",\$0); print \$0 }" ${PWD}/bing-cron > ${PWD}/tmp.tmp
+mv ${PWD}/tmp.tmp ${PWD}/bing-cron
+gawk "{ gsub(\"/DOWNLOAD_DIR\", \"${PWD}\",\$0); print \$0 }" ${PWD}/bing-wallpaper.sh > ${PWD}/tmp.tmp
+mv ${PWD}/tmp.tmp ${PWD}/bing-wallpaper.sh
+chmod a+x ${PWD}/bing-wallpaper.sh
 
 # setup crontab
-crontab bing-cron
+crontab ${PWD}/bing-cron
 
 # and run bing-wallpaper.sh once
-./bing-wallpaper.sh
+${PWD}/bing-wallpaper.sh
